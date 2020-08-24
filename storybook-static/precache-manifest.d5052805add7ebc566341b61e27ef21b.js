f79defea464abb73454d1d9c14575ba367701783
sy-fantienan <fantienan@forestar.com.cn>
2020-08-24 14:55:07 +0800

    feat: test travis cicd

---
+++ storybook-static/precache-manifest.d5052805add7ebc566341b61e27ef21b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "decf4ac89062edd4fb3017fd3bf45bb8",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.203700e5abaaa7626f69.bundle.js"
+  },
+  {
+    "url": "runtime~main.203700e5abaaa7626f69.bundle.js"
+  },
+  {
+    "url": "vendors~main.203700e5abaaa7626f69.bundle.js"
+  },
+  {
+    "url": "vendors~main.203700e5abaaa7626f69.bundle.js.LICENSE.txt"
+  }
+]);
