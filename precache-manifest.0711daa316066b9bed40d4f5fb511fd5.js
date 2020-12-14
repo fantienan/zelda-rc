3a8f67f294ba8dccff621b8422feeb7c3dcd4d0b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-14 07:59:44 +0000

    Deploy fantienan/zelda-rc to github.com/fantienan/zelda-rc.git:gh-pages

---
+++ precache-manifest.0711daa316066b9bed40d4f5fb511fd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821c9cc9357fbfac656c98e6401c56d1",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.77583af7e95bde486893.bundle.js"
+  },
+  {
+    "url": "runtime~main.77583af7e95bde486893.bundle.js"
+  },
+  {
+    "revision": "f2d5307001cdce75d991",
+    "url": "static/css/main.4e880f68.chunk.css"
+  },
+  {
+    "url": "vendors~main.77583af7e95bde486893.bundle.js"
+  },
+  {
+    "url": "vendors~main.77583af7e95bde486893.bundle.js.LICENSE.txt"
+  }
+]);
